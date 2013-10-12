c48d3e2ccbc1ceecd7eb7b8dc9e9f9dfc9d7bc2c
JCBuck <JCBuck@users.noreply.github.com>
2013-10-12 16:43:26 -0400

    Update searchReview.js

--- searchReview.js
+++ searchReview.js
@@ -15,7 +15,7 @@ javascript:(function(){
 	d.className = 'prod-search';
 	d.onclick = function(){if(d.parentNode!=null) d.parentNode.removeChild(d)};
 	d.setAttribute('style','position: fixed; top: 0px; left: 0px; background-color: #CCCCCC; z-index:9999999');
-	d.innerHTML="<a onclick=this.parentNode.parentNode.removeChild(this.parentNode)>Close me (or just click on the gray background)</a><br><br><a href="+srch+" target=_blank><u>Search google for more <b>" + y +"</b> reviews</u> (" + srch.split(encodeURI(' ')).length + " search terms)<br><i>(Warning: google search limits to 32 terms, some review sites may not be filtered)<br>(For query editing flexibility, I limited it to 40 so you can edit at google&#32;s search bar,<br>also some sites may have multiple reviews but get filtered out)</i></a>";
+	d.innerHTML="<a onclick=this.parentNode.parentNode.removeChild(this.parentNode)>Close me (or just click on the gray background)</a><br><br><a href="+srch+" target=_blank><u>Search google for more <b>" + y +"</b> reviews</u> (" + srch.split(encodeURI(' ')).length + " search terms)<br><i>(Warning: google search limits to 32 terms, some review sites may not be filtered)<br>(For query editing flexibility, I limited it to 40 so you can edit at google&#39;s search bar,<br>also some sites may have multiple reviews but get filtered out)</i></a>";
 	document.body.appendChild(d);
 		
 	}
