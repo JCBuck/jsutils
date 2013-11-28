9bb06b674cd2e7e21d47b4a5011d9b9e6b883145
JCBuck <JCBuck@users.noreply.github.com>
2013-11-28 03:09:28 -0500

    Update dupBudESEA.js

--- dupBudESEA.js
+++ dupBudESEA.js
@@ -1,1 +1,16 @@
-if((bl=document.querySelector("#getBuddies a"))!=null) bl.onclick(); function getBLArr() { var bl = document.querySelectorAll("table#buddyList tbody tr a.buddy-link"); for(var i =0, buds = []; i < bl.length; i++) buds.push(bl[i].text); return buds;}; setTimeout(function(){buds = getBLArr(); document.querySelector("#getBuddied a").onclick(); setTimeout(function(){budd = getBLArr()},250); console.log(buds.filter(function(b,i){return budd.indexOf(b)==-1;}));},250);
+if((bl=document.querySelector("#getBuddies a"))!=null) bl.onclick();
+
+function getBLArr() { 
+  var bl = document.querySelectorAll("table#buddyList tbody tr a.buddy-link"); 
+  for(var i =0, buds = []; i < bl.length; i++) 
+    buds.push(bl[i].text); 
+    return buds;
+  };
+  
+setTimeout(
+      function(){
+        buds = getBLArr();
+        document.querySelector("#getBuddied a").onclick();
+        setTimeout(function(){budd = getBLArr()},250);
+        console.log(buds.filter(function(b,i){return budd.indexOf(b)==-1;}));
+      },250);
