39281da55be25167828d9cb44766a91645c55334
JCBuck <JCBuck@users.noreply.github.com>
2014-01-10 15:39:05 -0500

    Update popupProducts.js

--- popupProducts.js
+++ popupProducts.js
@@ -3,7 +3,7 @@ javascript:(function(){
 	srch = document.getElementById("nav-search-input");
 	srch.oninput = 
 		function() { 
-			clearTimeout(srch);
+			clearTimeout(timeout);
 			timeout = setTimeout(
 					function (){
 						console.log("Search now! query: "+srch.value);
