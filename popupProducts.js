8119eef6961e414e985d32ac00eeac6d8e6059f8
JCBuck <JCBuck@users.noreply.github.com>
2014-01-11 17:28:31 -0500

    Update popupProducts.js

--- popupProducts.js
+++ popupProducts.js
@@ -36,7 +36,7 @@ javascript:(function(){
                   var xmlDoc=parser.parseFromString(mys,"text/html");
                   popres.innerHTML = "<div class=\"categories-modal-left hover-white\" style=\"overflow: overlay; width: 420px\">"+xmlDoc.getElementById("gdgt-result").innerHTML.replace("<ul>","<ul style=\"padding: 0\">").replace(/<img/g, "<img style=\"vertical-align: middle; height: 32px; width: 32px\" ").replace(/<a/g, "<a style=\"height: 40px; width:400px \" ") + "</div>";
                     }
-                  }
+                  };
                   xhr.open("GET", "http://www.engadget.com/search/?q="+encodeURIComponent(srch.value)+"&rail=rail", true);  
                   xhr.send(null);
                   
