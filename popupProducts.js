0a766797fcbb546ae074d811a0b2e65aac903af7
JCBuck <JCBuck@users.noreply.github.com>
2014-01-11 16:56:13 -0500

    Update popupProducts.js

--- popupProducts.js
+++ popupProducts.js
@@ -31,31 +31,6 @@ javascript:(function(){
                   xhr.open("GET", "http://www.engadget.com/search/?q="+encodeURIComponent(srch.value)+"&rail=rail", false);  
                   xhr.send(null);
                   var mys=xhr.responseText;
-                    /*
-                  var jsCode = document.createElement('a');   
-                  jsCode.innerHTML = "popupProducts";
-                  jsCode.href=mys;
-                  jsCode.className = "cool"
-                  document.body.appendChild(jsCode);
-            <div style="position:fixed; left:50%; margin-left:190px;z-index:99999;top:38px"><div class="categories-modal-left category-icon-25 hover-white">
-            <h4>search</h4>
-            <ul style="padding: 0">
-                    <li class="cellphones">
-                  <a href="http://www.engadget.com/reviews/cellphones/">
-                    <span></span>
-
-                    cellphones
-                  </a>
-                </li>
-                    <li class="laptops">
-                  <a href="http://www.engadget.com/reviews/laptops/">
-                    <span></span>
-
-                    laptops / portables
-                  </a>
-                </li>
-
-                </ul>*/
                   var parser=new DOMParser();
                   var xmlDoc=parser.parseFromString(mys,"text/html");
                   popres.innerHTML = "<div class=\"categories-modal-left hover-white\" style=\"overflow: overlay; width: 420px\">"+xmlDoc.getElementById("gdgt-result").innerHTML.replace("<ul>","<ul style=\"padding: 0\">").replace(/<img/g, "<img style=\"vertical-align: middle; height: 32px; width: 32px\" ").replace(/<a/g, "<a style=\"height: 40px; \" ") + "</div>";
