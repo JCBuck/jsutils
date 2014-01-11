d668bc0ba64861e5b6aa5a11a7820a53857d0268
JCBuck <JCBuck@users.noreply.github.com>
2014-01-11 02:22:23 -0500

    Update popupProducts.js

--- popupProducts.js
+++ popupProducts.js
@@ -1,6 +1,10 @@
 javascript:(function(){
 	var timeout;
-	srch = document.getElementById("nav-search-input");
+	var srch = document.getElementById("nav-search-input");
+	var gdgtiframe = document.createElement("iframe");
+	gdgtiframe.style.display = "none";
+	document.body.appendChild(gdgtiframe);
+	
 	srch.oninput = 
 		function() { 
 			clearTimeout(timeout);
@@ -9,7 +13,20 @@ javascript:(function(){
 						console.log("Search now! query: "+srch.value);
 						if(srch.value.trim.length==0) {
 							console.log("Except don't because spaces");
+							return;
 						}
+						
+					    gdgtiframe.src="http://www.engadget.com/search/?q="+encodeURIComponent(srch.value);
+					    $(gdgtiframe.contentDocument).bind('DOMNodeInserted', function(e) {
+					    	var element = e.target;
+						    if(element.id=="gdgt-result"){
+						    	console.log(element.innerText);	
+						    }
+					    }
+					    );
+    }, 2000);
+});
+						
 							
 					
 			},250);
