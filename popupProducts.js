225940a57348fa3ab7676c5e4d06e9bdbfbcd6a0
JCBuck <JCBuck@users.noreply.github.com>
2014-01-11 17:22:33 -0500

    Update popupProducts.js

--- popupProducts.js
+++ popupProducts.js
@@ -21,19 +21,27 @@ javascript:(function(){
             timeout = setTimeout(
                     function (){
                         console.log("Search now! query: "+srch.value);
-                        if(srch.value.trim().length==0) {
-                            console.log("Except don't because spaces");
+                        if(srch.value.trim().length<2) {
+                            console.log("Except don't because spaces or not long enough");
                             return;
                         }
                         
                        
                   var xhr = new XMLHttpRequest();
-                  xhr.open("GET", "http://www.engadget.com/search/?q="+encodeURIComponent(srch.value)+"&rail=rail", false);  
-                  xhr.send(null);
-                  var mys=xhr.responseText;
+                  xhr.onreadystatechange=function()  {
+                  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                    {
+                     var mys=xmlhttp.responseText;
                   var parser=new DOMParser();
                   var xmlDoc=parser.parseFromString(mys,"text/html");
-                  popres.innerHTML = "<div class=\"categories-modal-left hover-white\" style=\"overflow: overlay; width: 420px\">"+xmlDoc.getElementById("gdgt-result").innerHTML.replace("<ul>","<ul style=\"padding: 0\">").replace(/<img/g, "<img style=\"vertical-align: middle; height: 32px; width: 32px\" ").replace(/<a/g, "<a style=\"height: 40px; \" ") + "</div>";
+                  popres.innerHTML = "<div class=\"categories-modal-left hover-white\" style=\"overflow: overlay; width: 420px\">"+xmlDoc.getElementById("gdgt-result").innerHTML.replace("<ul>","<ul style=\"padding: 0\">").replace(/<img/g, "<img style=\"vertical-align: middle; height: 32px; width: 32px\" ").replace(/<a/g, "<a style=\"height: 40px; width:400px \" ") + "</div>";
+                    }
+                  }
+                  xhr.open("GET", "http://www.engadget.com/search/?q="+encodeURIComponent(srch.value)+"&rail=rail", false);  
+                  xhr.send(null);
+                  
+                  
+                 
             },250);
         };
 }());
